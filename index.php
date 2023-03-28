b55fb66f82f58ff45cba1847a459f839792e18ce
shee-la <sheilayvonne45@gmail.com>
2023-03-28 14:15:02 +0300

    commit of index file

---
+++ index.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+ <!-- link CSS -->
+        <link rel="stylesheet" href="style.css">
+    <title>signupform</title>
+</head>
+
+<body>
+    <div class="container my-5 ">
+        <div class="row">
+            <div class="col-md-12 col-xm-12">
+                <!--sigup screen takes 6(takes complete 50% of the screen) and small screen 12(takes complete 100% of the screen)-->
+                <h1 class="text-center my-5">SIGN-UP</h1>
+                <form action="signuplogic.php" method="post">
+                    <div class="mb-3">
+                        <label for="username" class="form-label">Username</label>
+                        <input type="text" id="username" class="form-control" placeholder="Enter Your Username"
+                            autocomplete="off" name="username">
+                    </div>
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Password</label>
+                        <input type="password" id="password" class="form-control" placeholder="Enter Your Password"
+                            autocomplete="off" name="password">
+                    </div>
+                    <div class="mb-3">
+                        <label for="cpassword" class="form-label">Confirm Password</label>
+                        <input type="password" id="cpassword" class="form-control" placeholder="Confirm Your Password"
+                            autocomplete="off" name="cpassword">
+                    </div>
+                    <button type="submit" class="btn btn-success w-100 my-3" name="signup">Sign-Up</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
